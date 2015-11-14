166bead85718efdaa1a9bb465990cc3b55b53d00
Erik Thiem <erikthiem@gmail.com>
2015-11-14 15:00:11 -0500

    improved landing page

--- duopoly/client/main.js
+++ duopoly/client/main.js
@@ -1,3 +1,9 @@
-if (Meteor.isClient) {
-  angular.module('duopoly',['angular-meteor']);
-}
+var ourApp = angular.module('duopoly',['angular-meteor']);
+
+ourApp.run(function($rootScope) {
+    $rootScope.appName = "Duopoly"; 
+});
+
+ourApp.controller('landing', function($scope) {
+
+});
