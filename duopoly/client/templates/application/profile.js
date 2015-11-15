0599299b76c916e783444762bc1e0a4f87171715
Ishmeet Grewal <ishmeetsinghgrewal@gmail.com>
2015-11-15 00:24:06 -0500

    Styling Fixes

--- duopoly/client/templates/application/profile.js
+++ duopoly/client/templates/application/profile.js
@@ -1,14 +1,11 @@
-<<<<<<< HEAD
 Template.profile.rendered = function () {
   $('.panel-title').text(Meteor.user().emails[0].address);
 };
 
 Template.profile.helpers({
-  
   goals: function() {
     return Goals.find();
   }
-  
 });
 
 Template.profile.events({
