90b19c969057439c420baabda4896de56398e6f6
Ishmeet Grewal <ishmeetsinghgrewal@gmail.com>
2016-04-23 20:27:34 -0400

    Added FAB

--- Goals.js
+++ Goals.js
@@ -1,6 +1,9 @@
 'use strict';
 
+import ActionButton from 'react-native-action-button';
+import Icon from 'react-native-vector-icons/Ionicons';
 var React = require('react-native');
+
 
 var {
   StyleSheet,
@@ -10,6 +13,14 @@ var {
 } = React;
 
 var styles = StyleSheet.create({
+  actionButton:{
+    marginBottom: 50
+  },
+  actionButtonIcon: {
+    fontSize: 20,
+    height: 22,
+    color: 'white',
+  },
   description:{
     fontSize: 20,
     backgroundColor: 'white'
@@ -29,6 +40,15 @@ class Goals extends Component {
         <Text style={styles.description}>
           Goals Tab
         </Text>
+
+        <ActionButton offsetY={75} buttonColor="rgba(231,76,60,1)">
+          <ActionButton.Item buttonColor='#9b59b6' title="New Goal" onPress={() => console.log("notes tapped!")}>
+            <Icon name="android-create" style={styles.actionButtonIcon} />
+          </ActionButton.Item>
+          <ActionButton.Item buttonColor='#3498db' title="Notifications" onPress={() => {}}>
+            <Icon name="android-notifications-none" style={styles.actionButtonIcon} />
+          </ActionButton.Item>
+        </ActionButton>
       </View>
     )
   }
